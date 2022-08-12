889115f7fc7c7c4404e27e267e637428d3459052
Marco Viana <marcoviana100@gmail.com>
2022-08-11 22:59:49 -0300

    Requizito 2 finalizado 2.0

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,8 +1,24 @@
-const fetchProducts = () => 
+/*
+Outra forma de fazer:
+ const fetchProducts = () => 
   // seu código aqui
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
  .then((respostaAPI) => respostaAPI.json())
  .catch((erro) => erro);
+ */
+  
+ const fetchProducts = async () => 
+ {
+  try {
+    const resposta = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    const exemplo1= await resposta.json();
+    return exemplo1;
+  } catch (error) {
+    return new Error('You must rovide an url')
+  }
+ 
+ }
+
 
 if (typeof module !== 'undefined') {
   module.exports = {
